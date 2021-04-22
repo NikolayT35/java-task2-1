e2b283985c4957f6eeb798afa6c1eae37717fa97
NikolayT35 <63493184+NikolayT35@users.noreply.github.com>
2021-04-22 09:28:13 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        int clientbill = 2_000_000_000;
-        int tranfer = 500_000_000;
+        int clientBill = 2_000_000_000;
+        int transfer = 500_000_000;
         int totalSum = clientbill + tranfer;
         System.out.println(totalSum);
     }
